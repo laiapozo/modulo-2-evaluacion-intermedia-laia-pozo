71bb7ca6461cb7aeed4decc2e52831b8da50cba4
laiapozo <pozotalarnlaia@gmail.com>
2025-03-04 09:31:32 +0100

    Add conditions to JS

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 /*
     - Seleccionar elementos html: input, button, span clue, span attempts
     - Crear accumulator
+    - Generar random number entre 1 y 100 (hacer console.log)
     - Cuando la usuaria haga click en el botón:
-        · Generar random number entre 1 y 100 (hacer console.log)
         · Guardar el valor del input number (hacer console.log)
         · Condiciones:
             > Si number user < 1 || number user > 100 --> Pintar en el span clue "El número debe estar entre 1 y 100"
@@ -18,4 +18,28 @@ const inputUserNumber = document.querySelector('.js-user-number');
 const pruebaButton = document.querySelector('.js-btn');
 const clue = document.querySelector('.js-clue');
 const attempts = document.querySelector('.js-attempts');
+
 let accumulator = 0;
+
+const getRandomNumber = (max) => Math.ceil(Math.random() * max);
+const randomNumber = getRandomNumber(100);
+console.log(`Random number: ${randomNumber}`);
+
+const handleClick = (event) => {
+    event.preventDefault();
+
+    const userNumber = parseInt(inputUserNumber.value);
+    console.log(`User number: ${userNumber}`);
+
+    if (userNumber < 1 || userNumber > 100) {
+        clue.innerHTML = 'El número debe estar entre 1 y 100';
+    } else if (userNumber === randomNumber) {
+        clue.innerHTML = 'Has ganado campeona!!!';
+    } else if (userNumber > randomNumber) {
+        clue.innerHTML = 'Demasiado alto';
+    } else if (userNumber < randomNumber) {
+        clue.innerHTML = 'Demasiado bajo';
+    }
+}
+
+pruebaButton.addEventListener("click", handleClick);
